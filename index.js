16795ac0480238ab6e0f27da62fd7e4a9a798032
André Díaz <andre290800@gmail.com>
2022-06-14 14:50:11 -0500

    Build the Customers API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const genres = require("./routes/genres");
+const customers = require("./routes/customers");
 
 mongoose
   .connect("mongodb://localhost/vidly")
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use("/api/genres", genres);
+app.use("/api/customers", customers);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
