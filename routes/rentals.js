9a11da4217007e9443b50b4ab4c317e6a3a8cc4c
André Díaz <andre290800@gmail.com>
2022-06-21 15:05:35 -0500

    Build the Rentals API

---
+++ routes/rentals.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const router = express.Router();
+const { Rental, validate } = require("../models/rental");
+const { Customer } = require("../models/customer");
+const { Movie } = require("../models/movie");
+
+router.get("/", async (req, res) => {
+  const rentals = await Rental.find().sort("-dateOut");
+  res.send(rentals);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const customer = await Customer.findById(req.body.customerId);
+  if (!customer) return res.status(400).send("Invalid customer ID.");
+
+  const movie = await Movie.findById(req.body.movieId);
+  if (!movie) return res.status(400).send("Invalid movie ID.");
+
+  if (movie.numberInStock === 0)
+    return res.status(400).send("Movie not in stock.");
+
+  const rental = new Rental({
+    customer: {
+      id: customer._id,
+      name: customer.name,
+      phone: customer.phone,
+    },
+    movie: {
+      _id: movie._id,
+      title: movie.title,
+      dailyRentalRate: movie.dailyRentalRate,
+    },
+  });
+  const result = await rental.save();
+
+  movie.numberInStock--;
+  await movie.save();
+
+  res.send(result);
+});
+
+router.get("/:id", async (req, res) => {
+  const rental = await Rental.findById(req.params.id);
+
+  if (!rental)
+    return res.status(404).send("The rental with the given ID was not found.");
+
+  res.send(rental);
+});
+
+router.delete("/:id", async (req, res) => {
+  const rental = await Rental.findByIdAndRemove(req.params.id);
+
+  if (!rental)
+    return res.status(404).send("The rental with the given ID was not found.");
+
+  res.send(rental);
+});
+
+module.exports = router;
