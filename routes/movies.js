026a2b173afded5ccd4334c5a4a7c11fd6a0302b
André Díaz <andre290800@gmail.com>
2022-06-21 15:06:11 -0500

    Fix not Awaiting Genre

--- routes/movies.js
+++ routes/movies.js
@@ -36,7 +36,7 @@ router.put("/:id", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const genre = Genre.findById(req.body.genreId);
+  const genre = await Genre.findById(req.body.genreId);
   if (!genre) return res.status(400).send("Invalid genre.");
 
   const { _id, name } = genre;
