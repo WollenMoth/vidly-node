f8d02b509d9d0e2df261b57c82b2626a15e7de01
André Díaz <andre290800@gmail.com>
2022-07-08 15:32:40 -0500

    Registering Users

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const { User, validate } = require("../models/user");
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already registered.");
+
+  user = new User(req.body);
+  await user.save();
+
+  res.send(user);
+});
+
+module.exports = router;
