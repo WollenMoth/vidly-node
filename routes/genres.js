ef88749fcdf882a4f624677f33ab905a7f56ac90
André Díaz <andre290800@gmail.com>
2022-07-28 22:06:13 -0500

    Removing Try Catch Blocks

--- routes/genres.js
+++ routes/genres.js
@@ -1,17 +1,17 @@
+const asyncMiddleware = require("../middleware/async");
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
 const express = require("express");
 const router = express.Router();
 const { Genre, validate } = require("../models/genre");
 
-router.get("/", async (req, res, next) => {
-  try {
+router.get(
+  "/",
+  asyncMiddleware(async (req, res) => {
     const genres = await Genre.find().sort("name");
     res.send(genres);
-  } catch (error) {
-    next(error);
-  }
-});
+  })
+);
 
 router.post("/", auth, async (req, res) => {
   const { error } = validate(req.body);
