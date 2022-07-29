fd4207de20ec74ce7cfa1229a4738432c7188569
André Díaz <andre290800@gmail.com>
2022-07-28 21:49:21 -0500

    Handling Rejected Promises

--- routes/genres.js
+++ routes/genres.js
@@ -5,9 +5,12 @@ const router = express.Router();
 const { Genre, validate } = require("../models/genre");
 
 router.get("/", async (req, res) => {
-  const genres = await Genre.find().sort("name");
-
-  res.send(genres);
+  try {
+    const genres = await Genre.find().sort("name");
+    res.send(genres);
+  } catch (error) {
+    res.status(500).send("Something failed.");
+  }
 });
 
 router.post("/", auth, async (req, res) => {
