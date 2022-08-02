591fa65e7cdcfaa5d7bd30bc58eb2402e52ee29c
André Díaz <andre290800@gmail.com>
2022-08-02 17:54:27 -0500

    Logging to MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("express-async-errors");
 const winston = require("winston");
+require("winston-mongodb");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const config = require("config");
@@ -14,6 +15,10 @@ const auth = require("./routes/auth");
 const error = require("./middleware/error");
 
 winston.add(winston.transports.File, { filename: "logfile.log" });
+winston.add(winston.transports.MongoDB, {
+  db: "mongodb://localhost/vidly",
+  level: "error",
+});
 
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
