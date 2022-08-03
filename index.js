6c45b87d4991148b79815324d5c654f3281d7def
André Díaz <andre290800@gmail.com>
2022-08-02 21:43:59 -0500

    Extracting the Db Logic

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ require("winston-mongodb");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const config = require("config");
-const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+
 require("./startup/routes")(app);
+require("./startup/db")();
 
 winston.handleExceptions(
   new winston.transports.File({ filename: "uncaughtExceptions.log" })
@@ -23,11 +24,6 @@ if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
   process.exit(1);
 }
-
-mongoose
-  .connect("mongodb://localhost/vidly")
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch(() => console.error("Could not connect to MongoDB..."));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
