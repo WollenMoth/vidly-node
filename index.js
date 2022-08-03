f94a4080b001594076ea53bfb059df5b62821177
André Díaz <andre290800@gmail.com>
2022-08-02 21:30:29 -0500

    Extracting Routes

--- index.js
+++ index.js
@@ -6,13 +6,8 @@ Joi.objectId = require("joi-objectid")(Joi);
 const config = require("config");
 const mongoose = require("mongoose");
 const express = require("express");
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
-const error = require("./middleware/error");
+const app = express();
+require("./startup/routes")(app);
 
 winston.handleExceptions(
   new winston.transports.File({ filename: "uncaughtExceptions.log" })
@@ -33,18 +28,6 @@ mongoose
   .connect("mongodb://localhost/vidly")
   .then(() => console.log("Connected to MongoDB..."))
   .catch(() => console.error("Could not connect to MongoDB..."));
-
-const app = express();
-
-app.use(express.json());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
