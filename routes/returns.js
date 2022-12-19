5a25464874f57d31a5d4f910fac1faa1f8e05209
André Díaz <andre290800@gmail.com>
2022-12-19 00:29:17 -0600

    Looking Up an Object

--- routes/returns.js
+++ routes/returns.js
@@ -2,10 +2,17 @@ const auth = require("../middleware/auth");
 const express = require("express");
 const router = express.Router();
 const { validate } = require("../models/return");
+const { Rental } = require("../models/rental");
 
 router.post("/", auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
+
+  const rental = await Rental.findOne({
+    "customer._id": req.body.customerId,
+    "movie._id": req.body.movieId,
+  });
+  if (!rental) return res.status(404).send("Rental not found.");
 });
 
 module.exports = router;
