f90c0ad9e0e9e6fe22640ec69149fe90bf891b40
André Díaz <andre290800@gmail.com>
2023-12-03 15:14:34 -0600

    fix: 🐛 update winston

--- startup/logging.js
+++ startup/logging.js
@@ -3,12 +3,12 @@ const winston = require("winston");
 require("express-async-errors");
 
 module.exports = function () {
-  winston.handleExceptions(
+  winston.exceptions.handle(
     new winston.transports.Console({ colorize: true, prettyPrint: true }),
     new winston.transports.File({ filename: "uncaughtExceptions.log" })
   );
 
-  winston.add(winston.transports.File, { filename: "logfile.log" });
+  winston.add(new winston.transports.File({ filename: "logfile.log" }));
   // winston.add(winston.transports.MongoDB, {
   //   db: "mongodb://localhost/vidly",
   //   level: "error",
